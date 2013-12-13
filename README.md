4d5d3d40cbc45518e504b658299bdcce871e0a28
Swaagie <info@martijnswaagman.nl>
2013-12-13 15:08:24 +0100

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Go BMI
+
+To try out Go-lang I build a tiny command line BMI calculator. Nothing fancy but
+enough to cover some basics. If you'd like to do something similar check out the
+[blog post](http://www.martijnswaagman.nl/go/)
+
+## License
+
+MIT
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import "os"
+import "log"
+import "fmt" 
+import "math"
+
+/*
+ * Prompt user and calculate BMI.
+ *
+ * @public
+ */
+func main() {
+  fmt.Println("\nCalculate your BMI\n")
+
+  weight := get("weight")
+  length := get("length")
+
+  fmt.Printf("\nYour BMI: %.2f kg/m² \n\n", weight / math.Pow(length/100, 2));
+}
+
+/*
+ * Prompt the user for their weight or length.
+ *
+ * @param {string} s what variable should the user be prompted with
+ * @private
+ */
+func get(s string) (float64) {
+  var x float64
+
+  fmt.Print("What is your "+ s +"? ")
+
+  _, err := fmt.Scanf("%f", &x)
+  check(err)
+
+  return x
+}
+
+/*
+ * Log and exit if an error is encountered.
+ *
+ * @param {error} err
+ * @private
+ */
+func check(err error) {
+  if err != nil {
+    log.Fatal(err)
+    os.Exit(0)
+  }
+}
